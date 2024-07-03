dbfea3687b910aabf06f096a58ff4e9fc0f3f009
mohammad hossein sangestani <mhsangestani@gmail.com>
2024-07-04 03:28:55 +0330

    The first task of season six
    + add filter for published_date
        - Take the time considered for the
            published_date section and
            compare it with the current time,
            if it is past the current time, it
             should be displayed, otherwise
            not.
    + add counter view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    # empty_value_display = '_empty_'
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def blog_view(request):
-        return render(request, 'blog/blog-home.html')
+    Post.objects.filter(published_date__lte=timezone.now()).update(status=True)
+    posts=Post.objects.filter(status=True)
+    context = {'posts':posts} 
+    return render(request, 'blog/blog-home.html', context)
 
 
-def blog_single_view(request):
-    return render(request, 'blog/blog-single.html') 
+def blog_single_view(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+
+    post.counted_views += 1
+    post.save()
+    context = {'post':post}
+    return render(request, 'blog/blog-single.html', context) 
